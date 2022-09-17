54e3af73cdddd8bb4b5e897e88ed21dfc5e5dfb6
Daniel Suarez <suarez.daniel101@gmail.com>
2022-09-16 23:02:03 -0400

    Done.

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,6 +1,11 @@
 class ArtistsController < ApplicationController
 
-  def new
+    def create
+      @artist = Artist.create(params.require(:artist).permit(:name, :bio))
+      redirect_to artist_path(@artist)
+    end
+  
+    def new
     @artist = Artist.new
   end
 
